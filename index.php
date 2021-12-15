0b9e87ee79060d6397669b9ad45c1d3579c1928a
DaanBuitenweg <566171@edu.rocmn.nl>
2021-12-15 11:27:33 +0100

    html naar php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css" />
+    <title>www.Hardstore.com</title>
+</head>
+<?php include_once 'functions.php' ?>
+<body>
+    <header>
+        <div>
+            <img src="./media/Hardstore.png" alt="logo" class= "logo"/>
+
+            <h2>Hardstore</h2>
+        </div>
+        <div>
+        <?php include 'navbar.php' ?>
+        </div>
+
+    </header>
+
+     </div>
+
+    <main>
+        <section class="presentation cart logo-container">
+            <div class="introduction">
+                <div class="intro-text">
+                <img src="./media/Winkelmandje.webp" alt="cart" />
+                    <h1></h1>
+                    <p>
+
+                    </p>
+                </div>
+                <div class="cta">
+                    <button class="cta-add">Check black-friday deals</button>
+                </div>
+            </div>
+            <div class="cover">
+                <img src="./media/blackfriday.webp" alt="black-friday deals" />
+            </div>
+        </section>
+
+
+
+
+    </main>
+</body>
+</html>
